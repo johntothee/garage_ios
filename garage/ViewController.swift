208edb269f2f23966bf26e508de19da02c00e6af
John Erhardt <johne.erhardt@gmail.com>
2018-02-21 15:42:48 -0700

    Initial Commit

---
+++ garage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  garage
+//
+//  Created by John Erhardt on 2/21/18.
+//  Copyright © 2018 John Erhardt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
